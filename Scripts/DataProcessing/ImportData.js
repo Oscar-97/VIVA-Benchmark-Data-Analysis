0a266500ea3443cdde73ab0836f1e6bb7b2d3786
Oscar <91684376+Oscar-97@users.noreply.github.com>
2022-11-28 11:31:19 +0200

    Updated variables.

--- Scripts/DataProcessing/ImportData.js
+++ Scripts/DataProcessing/ImportData.js
@@ -1,34 +1,21 @@
+// Regarding load a local file:
 // https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS/Errors/CORSRequestNotHttp
 
-/*
-Loading a local file
-
-Local files from the same directory and subdirectories were historically treated as being from the same origin. 
-This meant that a file and all its resources could be loaded from a local directory or subdirectory during testing, without triggering a CORS error.
-
-Unfortunately this had security implications, as noted in this advisory: CVE-2019-11730. 
-Many browsers, including Firefox and Chrome, now treat all local files as having opaque origins (by default). 
-As a result, loading a local file with included local resources will now result in CORS errors.
-
-Developers who need to perform local testing should now set up a local server. 
-As all files are served from the same scheme and domain (localhost) they all have the same origin, and do not trigger cross-origin errors. 
-*/
-
-export function ImportData(filePath) {
-    // TEMP, using XMLHttpRequest. 
+export function ImportData(FilePath) {
+    // Using XMLHttpRequest. 
     // Replace at a later stage where user can upload the file from their directory.
-    let rawData = new XMLHttpRequest();
-    rawData.open("GET", filePath, false);
-    rawData.onreadystatechange = function () {
-        if(rawData.readyState === 4)
+    const RawData = new XMLHttpRequest();
+    RawData.open("GET", FilePath, false);
+    RawData.onreadystatechange = function () {
+        if(RawData.readyState === 4)
         {
-            if(rawData.status === 200 || rawData.status == 0)
+            if(RawData.status === 200 || RawData.status == 0)
             {
               console.log("Loaded data.")
             }
         }
     }
-    rawData.send(null);
-    let returnData = rawData.responseText.split("\n");
-    return returnData;
+    RawData.send(null);
+    const ReturnData = RawData.responseText.split("\n");
+    return ReturnData;
 }
