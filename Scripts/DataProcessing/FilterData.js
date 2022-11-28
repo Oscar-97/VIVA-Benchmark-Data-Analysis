2763d3471635c7646e325d91eccf20dfdea584b7
Oscar <91684376+Oscar-97@users.noreply.github.com>
2022-11-28 11:21:40 +0200

    Added comments.

--- Scripts/DataProcessing/FilterData.js
+++ Scripts/DataProcessing/FilterData.js
@@ -1,50 +1,53 @@
 // Filter the instance and solvers.
 export function GetInstanceAndSolvers(data) {
-    let tempData1 = data.split("\|");
-    let tempData2 = tempData1.filter(item => item);
-    for (var i = tempData2.length -1; i >= 0; i--){
-        tempData2[i] = tempData2[i].replace(/\s+/g,"");
+    let TempData1 = data.split("\|");
+    let TempData2 = TempData1.filter(item => item);
+    for (var i = TempData2.length - 1; i >= 0; i--) {
+        TempData2[i] = TempData2[i].replace(/\s+/g, "");
     }
-    let returnData = tempData2;
-    return returnData.slice(0, -1);
+    let ReturnData = TempData2;
+    return ReturnData.slice(0, -1);
 }
 
-// Filter the data labels
+// Filter the data labels.
 export function GetDataLabels(data) {
     // Get the solver names.
-    let returnData = data.split(" ");
+    let ReturnData = data.split(" ");
     // Remove extra 'bound' in array when splitting.
-    let tempData = returnData.filter(function(value) {
-        return value != "bound"; });
+    let TempData = ReturnData.filter(function (value) {
+        return value != "bound";
+    });
     // Remove "|" and "#".
-    let tempData2 = tempData.map(function(value) {
-        return value.replace(/[I| #]/g, ''); });
+    let TempData2 = TempData.map(function (value) {
+        return value.replace(/[I| #]/g, '');
+    });
     // Remove blanks.
-    let tempData3 = tempData2.filter(item => item);
-    
-    // Add back 'bound' with function, and clean up the rest of the content.
-    for (var i = 0; i < tempData3.length; i++)
-    if (tempData3[i] === "Primal") {
-        tempData3[i] = "Primal bound";
-    } else if (tempData3[i] === "Dual"){
-        tempData3[i] = "Dual bound"
-    } else if (tempData3[i] === "Nodes"){
-        tempData3[i] = "Nodes I"
-    }
-    returnData = tempData3;
+    let TempData3 = TempData2.filter(item => item);
+
+    // Add back 'bound/I' with function, and clean up the rest of the content.
+    for (var i = 0; i < TempData3.length; i++)
+        if (TempData3[i] === "Primal") {
+            TempData3[i] = "Primal bound";
+        } else if (TempData3[i] === "Dual") {
+            TempData3[i] = "Dual bound";
+        } else if (TempData3[i] === "Nodes") {
+            TempData3[i] = "Nodes I";
+        }
+    ReturnData = TempData3;
     // Remove last /r in array before returning the data.
-    return returnData.slice(0, -1);
+    return ReturnData.slice(0, -1);
 }
 
 // Filter the problems.
 export function GetProblems(data) {
-    let returnData = data.split(" ");
+    let ReturnData = data.split(" ");
     // Remove blanks.
-    let tempData = returnData.filter(item => item);
+    let TempData = ReturnData.filter(item => item);
     // Remove "|".
-    let tempData2 = tempData.map(function(value) {
-        return value.replace(/[|]/g, ''); });
-    returnData = tempData2;
+    let TempData2 = TempData.map(function (value) {
+        return value.replace(/[|]/g, '');
+    });
+    ReturnData = TempData2;
     // Remove last /r in array and return data.
-    return returnData.slice(0, -1);
+    return ReturnData.slice(0, -1);
 }
