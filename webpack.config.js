0fb66f184b1811df31785376626c18a275cddd46
Oscar <91684376+Oscar-97@users.noreply.github.com>
2022-12-09 19:11:47 +0200

    Added webpack for usage without live server.

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    entry: './Src/Scripts/Main.js', // The entry point for your code
+    output: {
+      filename: 'bundle.js', // The name of the bundled file
+      path: __dirname + '/Dist', // The directory where the bundled file should be saved
+    },
+  };
+  
