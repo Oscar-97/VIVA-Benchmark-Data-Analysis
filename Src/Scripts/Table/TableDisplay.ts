938f6d0c403a06999e4e442f7186ddba7f7830de
Oscar <91684376+Oscar-97@users.noreply.github.com>
2023-01-09 14:50:37 +0200

    Change disabled status on display.

--- Src/Scripts/Table/TableDisplay.ts
+++ Src/Scripts/Table/TableDisplay.ts
@@ -1,15 +1,13 @@
 const jq = require('jquery');
 const DataTables = require('datatables.net-bs5');
 const Buttons = require('datatables.net-buttons-bs5');
-// const ButtonsColVis = require('datatables.net-buttons/js/buttons.colVis.js');
-// const ColReorder = require('datatables.net-colreorder-bs5');
 const RowReorder = require('datatables.net-rowreorder-bs5');
 const Select = require('datatables.net-select-bs5');
 
 import { GetCheckedSolvers, GetComparisonArray } from "../Solvers/UsedSolvers";
 import { TableData } from "./TableData";
 
-import { SelectAllButton, FilterSelectionButton } from '../Elements/Elements';
+import { SelectAllButton, FilterSelectionButton, SaveLocalStorageButton, DownloadCSVButtonLayer } from '../Elements/Elements';
 
 export function TableDisplay(Instance: string, Solvers: string[], InstanceLabels: string | any[], DataLabels: any[], ProblemList: string | any[], ResultsData: any[]) {
     let CheckedSolvers = GetCheckedSolvers();
@@ -27,14 +25,8 @@ export function TableDisplay(Instance: string, Solvers: string[], InstanceLabels
     // @ts-ignore
     jq("#dataTableGenerated").DataTable(
         {
-            // colReorder: true,
             rowReorder: true,
             buttons: ['copy', 'csv'],
-            // columnDefs: [ {
-            //     orderable: false,
-            //     className: 'select-checkbox',
-            //     targets: 0
-            //     } ],
             select: {
                 style: 'os',
                 blurable: true,
@@ -44,4 +36,6 @@ export function TableDisplay(Instance: string, Solvers: string[], InstanceLabels
     );
     // Enable the selection button after displaying.
     FilterSelectionButton.disabled = false;
+    SaveLocalStorageButton.disabled = false;
+    DownloadCSVButtonLayer.disabled = false;
 }
