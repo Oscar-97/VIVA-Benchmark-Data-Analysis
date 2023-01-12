562e84d3713c0372e079077cd039970bed602e57
Oscar <91684376+Oscar-97@users.noreply.github.com>
2023-01-12 21:03:13 +0200

    Initial version of create data.

---
+++ Src/Scripts/DataProcessing/CreateData.ts
@@ -0,0 +1,10 @@
+export function CreateData(Instance:string, Solvers: any[], InstanceLabels: any[], DataLabels: any[], ProblemList: any[], ResultsData: any[]) {
+    let output: any[] = [];
+    let delimiter = " ";
+    output.push([Instance].concat(Solvers));
+    output.push([InstanceLabels.join(delimiter)].concat(DataLabels));
+    console.log("Output: ", output);
+    console.log("Add problemlist and data....")
+    return output;
+}
+
