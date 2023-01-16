a9c48adb1f72d3c6d580066f493efe13afc0670a
Oscar <91684376+Oscar-97@users.noreply.github.com>
2023-01-16 15:44:05 +0200

    Added badge element to list.

--- Src/Scripts/Elements/Elements.ts
+++ Src/Scripts/Elements/Elements.ts
@@ -20,6 +20,7 @@ export const SelectAllButton = document.getElementById("selectAllButton") as HTM
  * @param DownloadCSVButton Download link the currently displayed table.
  * @param DownloadCSVButtonLayer Button element layer for the link.
  * @param InputSearch Input for searching for problems.
+ * @param SolverAndProblemsHeader Badge with the text Solvers or Problems.
  */
 export const ViewSelectionButton = document.getElementById("viewSelectionButton") as HTMLButtonElement;
 export const FilterSelectionButton = document.getElementById("filterSelectionButton") as HTMLButtonElement;
@@ -27,6 +28,7 @@ export const SaveLocalStorageButton = document.getElementById("saveLocalStorage"
 export const DownloadCSVButton = document.getElementById("downloadCSVButton") as HTMLAnchorElement;
 export const DownloadCSVButtonLayer = document.getElementById("downloadCSVButtonLayer") as HTMLButtonElement;
 export const InputSearch = document.getElementById("tableSearch") as HTMLInputElement;
+export const SolverAndProblemsHeader = document.getElementById('SolverAndProblemsHeader') as HTMLSpanElement;
 
 /**
  * Plots page.
