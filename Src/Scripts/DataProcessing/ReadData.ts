ef7cb4ecd2e6209aaa1da10dd5e92bf9318b8825
Oscar <91684376+Oscar-97@users.noreply.github.com>
2023-02-07 16:08:23 +0200

    Support for multiple file uploads.

--- Src/Scripts/DataProcessing/ReadData.ts
+++ Src/Scripts/DataProcessing/ReadData.ts
@@ -1,29 +1,34 @@
 import { FileInput, ImportDataButton } from "../Elements/Elements";
+import { DisplayErrorNotification } from "../Elements/DisplayAlertNotification";
 
 export function ReadData(RawData: any[]) {
     ImportDataButton.disabled = false;
-    let Reader = new FileReader();
-    let File = FileInput.files[0];
-    let FileName = File.name;
-    let FileExtension = FileName.split('.').pop();
-    Reader.addEventListener('load', function () {
-        RawData.length = 0;
-        if (FileExtension === "txt" || FileExtension === "trc") {
-            // Split the file's text into an array of lines.
-            let lines = (<string>Reader.result).split('\r\n');
-            // Iterate over the lines array and process each line as needed.
-            for (let i = 0; i <= lines.length - 1; i++) {
-                let line = lines[i];
-                RawData.push(line);
-            }
-        } else {
-            console.log("Invalid file extension. Please use a .txt or .trc file.");
-            return;
-        }
 
-    });
-    // Read the file as text.
-    Reader.readAsText(FileInput.files[0]);
+    for (let i = 0; i < FileInput.files.length; i++) {
+      let Reader = new FileReader();
+      let File = FileInput.files[i];
+      let FileName = File.name;
+      let FileExtension = FileName.split('.').pop();
+  
+      Reader.addEventListener('load', function () {
+        if (FileExtension === "txt" || FileExtension === "trc") {
+          // Split the file's text into an array of lines.
+          let lines = (<string>Reader.result).split('\r\n');
+          // Iterate over the lines array and process each line as needed.
+          for (let i = 0; i <= lines.length - 1; i++) {
+            let line = lines[i];
+            RawData.push(line);
+          }
+        } else {
+          console.log("Invalid file extension. Please use a .txt or .trc file.");
+          DisplayErrorNotification("Invalid file extension. Please use a .txt or .trc file.");
+        }
+      });
+  
+      // Read the file as text.
+      Reader.readAsText(File);
+    }
+  
     return RawData;
 }
 
