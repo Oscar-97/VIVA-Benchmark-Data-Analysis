3c3fbf04be2310f31b216771efc3cb23f741b826
Oscar <91684376+Oscar-97@users.noreply.github.com>
2023-02-24 14:41:16 +0200

    Renamed.

--- Src/Scripts/DataProcessing/GetInstanceInformation.ts
+++ Src/Scripts/DataProcessing/GetInstanceInformation.ts
@@ -11,14 +11,20 @@ export function GetInstanceInformation(
 
   for (let i = 1; i < RawInstanceInfoData.length; i++) {
     const Obj = {};
-    const currentLine = RawInstanceInfoData[i].split(";");
+    const CurrentLine = RawInstanceInfoData[i].split(";");
     // "Set value to empty if it's not existing."
     for (let j = 0; j < Header.length; j++) {
-      Obj[Header[j]] = currentLine[j] || "";
+      Obj[Header[j]] = CurrentLine[j] || "";
     }
     InstanceInfo.push(Obj);
   }
   DisplayAlertNotification("Instance information succesfully loaded!");
   console.log("Instance info: ", InstanceInfo);
   return InstanceInfo;
+}
+
+export function GetInstancePrimalDualbounds(RawSoluData) {
+  const SoluData = RawSoluData;
+  console.log("To be implemented.");
+  return SoluData;
 }
