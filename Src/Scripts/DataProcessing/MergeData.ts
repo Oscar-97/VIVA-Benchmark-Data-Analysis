0e234ff083e5153ee19f1030b466e68b7209acbe
Oscar <91684376+Oscar-97@users.noreply.github.com>
2023-02-24 14:42:36 +0200

    Updated object key name.

--- Src/Scripts/DataProcessing/MergeData.ts
+++ Src/Scripts/DataProcessing/MergeData.ts
@@ -9,14 +9,18 @@ export function MergeData(TrcData: any[], InstanceData: any[]): string[] {
 
   for (const obj1 of TrcData) {
     for (const obj2 of InstanceData) {
-      if (obj1.filename === obj2.name) {
+      if (obj1.InputFileName === obj2.name) {
         console.log("found.");
-        const mergedObj = Object.assign({}, obj1, obj2);
-        MergedData.push(mergedObj);
+        const MergedObj = Object.assign({}, obj1, obj2);
+        MergedData.push(MergedObj);
         break;
       }
     }
   }
   console.log("MergedData", MergedData);
+
+  if (MergedData.length === 0) {
+    throw new Error("No matching filenames found in both arrays.");
+  }
   return MergedData;
 }
