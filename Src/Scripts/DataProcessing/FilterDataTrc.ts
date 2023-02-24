8abaefc7ab138bbf2f97a399dd0e30d0edae1e7d
Oscar <91684376+Oscar-97@users.noreply.github.com>
2023-02-24 14:43:15 +0200

    Initial extra columns calculation.

--- Src/Scripts/DataProcessing/FilterDataTrc.ts
+++ Src/Scripts/DataProcessing/FilterDataTrc.ts
@@ -1,3 +1,10 @@
+import {
+  CalculateDirection,
+  CalculatePrimalBound,
+  CalculateDualBound,
+  SetTermStatus,
+} from "./CalculateResults";
+
 /**
  * Extract the data from the trc file.
  * @param RawData The provided raw data.
@@ -9,6 +16,7 @@ export function ExtractTrcData(RawData: string[]): string[] {
 
   /**
    * Check if headers are included in the .trc file. If they are found, include them, if not, use the custom headers.
+   * TODO: Skip custom headers overall.
    */
   if (FirstLine[0].startsWith("*")) {
     console.log("Found headers.");
@@ -33,49 +41,86 @@ export function ExtractTrcData(RawData: string[]): string[] {
      * Standard order of headers:
      * https://www.gamsworld.org/performance/trace.htm
      */
-    //const defaultHeaders = ["InputFileName", "ModelType", "SolverName", "Direction",
-    //"ModelStatus", "SolverStatus", "ObjectiveValue", "SolverTime"];
     const DefaultHeaders = [
-      "filename",
-      "modeltype",
-      "solvername",
-      "NLP def",
-      "MIP def",
-      "juliantoday",
-      "direction",
-      "equnum",
-      "varnum",
-      "dvarnum",
-      "nz",
-      "nlnz",
-      "optfile",
-      "modelstatus",
-      "solvestatus",
-      "obj",
-      "objest",
-      "res used",
-      "iter used",
-      "dom used",
-      "nodes used",
-      "user1",
+      "InputFileName",
+      "ModelType",
+      "SolverName",
+      "NLP",
+      "MIP",
+      "JulianDate",
+      "Dir",
+      "Equs",
+      "Vars",
+      "Disc",
+      "NumberOfNonZeros",
+      "NumberOfNonlinearNonZeros",
+      "OptionFile",
+      "ModelStatus",
+      "TermStatus",
+      "PrimalBoundSolver",
+      "DualBoundSolver",
+      "Time[s]",
+      "NumberOfIterations",
+      "NumberOfDomainViolations",
+      "Nodes[i]",
+      "UserComment",
     ];
     const PreviousRow = {};
 
     for (let i = 0; i < RawData.length; i++) {
       const CurrentLine = RawData[i].split(",");
-      const Filename = CurrentLine[0];
-      const Solvername = CurrentLine[2];
+      const FileName = CurrentLine[0];
+      const SolverName = CurrentLine[2];
 
       // Check if the combination of filename and solver has been used earlier.
-      if (PreviousRow[Filename] === Solvername) {
+      if (PreviousRow[FileName] === SolverName) {
         continue;
       }
-      PreviousRow[Filename] = Solvername;
+      PreviousRow[FileName] = SolverName;
 
       const Obj = {};
       for (let j = 0; j < DefaultHeaders.length; j++) {
         Obj[DefaultHeaders[j]] = CurrentLine[j];
       }
+      /**
+       * Modify keys.
+       */
+      if ("Dir" in Obj) {
+        const CurrentDirValue = Obj.Dir;
+        const NewDirValue = CalculateDirection(CurrentDirValue);
+        Obj.Dir = NewDirValue;
+      }
+
+      if ("PrimalBoundSolver" in Obj) {
+        const CurrentPrimalBoundValue = Obj.PrimalBoundSolver;
+        const CurrentDirValue = Obj["Dir"];
+        const NewPrimalBoundValue = CalculatePrimalBound(
+          CurrentPrimalBoundValue,
+          CurrentDirValue
+        );
+        Obj.PrimalBoundSolver = NewPrimalBoundValue;
+      }
+
+      if ("DualBoundSolver" in Obj) {
+        const CurrentDualBoundValue = Obj.DualBoundSolver;
+        const CurrentDirValue = Obj["Dir"];
+        const NewDualBoundValue = CalculateDualBound(
+          CurrentDualBoundValue,
+          CurrentDirValue
+        );
+        Obj.DualBoundSolver = NewDualBoundValue;
+      }
+
+      if ("TermStatus" in Obj) {
+        const CurrentTermValue = Obj.TermStatus;
+        const NewTermValue = SetTermStatus(CurrentTermValue);
+        Obj.TermStatus = NewTermValue;
+      }
+
+      /**
+       * Create new keys for:
+       */
+
       TrcData.push(Obj);
     }
   }
