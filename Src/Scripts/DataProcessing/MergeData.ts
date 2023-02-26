7f5a9d5c1496284ad7608bdbe82f01fd8d65a4c2
Oscar Härtull <oscar.hartull@gmail.com>
2023-02-26 14:16:25 +0200

    Merge solu and instance data.

--- Src/Scripts/DataProcessing/MergeData.ts
+++ Src/Scripts/DataProcessing/MergeData.ts
@@ -4,20 +4,48 @@
  * @param InstanceData Instance information data.
  * @returns MergedData
  */
-export function MergeData(TrcData: any[], InstanceData: any[]): string[] {
+export function MergeInstanceData(
+  TrcData: any[],
+  InstanceData: any[]
+): string[] {
   const MergedData = [];
 
   for (const obj1 of TrcData) {
     for (const obj2 of InstanceData) {
       if (obj1.InputFileName === obj2.name) {
-        console.log("found.");
         const MergedObj = Object.assign({}, obj1, obj2);
         MergedData.push(MergedObj);
         break;
       }
     }
   }
-  console.log("MergedData", MergedData);
+  console.log("TrcData with instanceinformation: ", MergedData);
+
+  if (MergedData.length === 0) {
+    console.log("No matching filenames found in both arrays.");
+  }
+  return MergedData;
+}
+
+/**
+ *
+ * @param TrcData
+ * @param SoluData
+ * @returns
+ */
+export function MergeSoluData(TrcData: any[], SoluData: any[]): string[] {
+  const MergedData = [];
+
+  for (const obj1 of TrcData) {
+    for (const obj2 of SoluData) {
+      if (obj1.InputFileName === obj2.InputFileName) {
+        const MergedObj = Object.assign({}, obj1, obj2);
+        MergedData.push(MergedObj);
+        break;
+      }
+    }
+  }
+  console.log("TrcData with Solu information: ", MergedData);
 
   if (MergedData.length === 0) {
     console.log("No matching filenames found in both arrays.");
