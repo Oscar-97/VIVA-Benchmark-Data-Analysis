78e3683a964fc6f46e1363c2ff4e77014d6eda97
Oscar <91684376+Oscar-97@users.noreply.github.com>
2023-02-27 14:45:10 +0200

    Custom state test.

--- Src/Scripts/DataTable/DataTableWrapper.ts
+++ Src/Scripts/DataTable/DataTableWrapper.ts
@@ -84,6 +84,11 @@ function DataTablesConfiguration(): void {
   const table = jq("#dataTableGenerated").DataTable({
     destroy: true,
     stateSave: true,
+    stateSaveKey: "DataTableSession",
+    stateLoadCallback: function() {
+      let customState = localStorage.getItem("DataTableSession");
+      return customState ? JSON.parse(customState) : null;
+    },
     dom:
       "<'row'<'col-sm-12 col-md-6'l><'col-sm-12 col-md-6'f>>" +
       "<'row'<'col-sm-12'tr>>" +
