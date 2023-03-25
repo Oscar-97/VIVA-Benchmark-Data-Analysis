50b77d5c14eef81110ee13c693a6ceab0cc0b2ce
Oscar <91684376+Oscar-97@users.noreply.github.com>
2023-03-25 17:43:44 +0200

    Chagned status on import data button.

--- Src/Scripts/Elements/ElementStatus.ts
+++ Src/Scripts/Elements/ElementStatus.ts
@@ -1,52 +1,52 @@
 import {
-    FileInput,
-    ImportDataButton,
-    ViewAllResultsButton,
-    FilterSelectionButton,
-    SelectAllButton,
-    SaveLocalStorageButton,
-    DownloadConfigurationButton,
-    DownloadConfigurationButtonLayer,
-    DownloadCSVButton,
-    DownloadCSVButtonLayer,
-    DeleteLocalStorageButton,
-    ClearTableButton,
-    ViewPlotsButton,
-  } from "./Elements";
+  FileInput,
+  ImportDataButton,
+  ViewAllResultsButton,
+  FilterSelectionButton,
+  SelectAllButton,
+  SaveLocalStorageButton,
+  DownloadConfigurationButton,
+  DownloadConfigurationButtonLayer,
+  DownloadCSVButton,
+  DownloadCSVButtonLayer,
+  DeleteLocalStorageButton,
+  ClearTableButton,
+  ViewPlotsButton,
+} from "./Elements";
 
 /**
  * Input values and button statuses when arriving/reloading to the table page.
  */
 export function ElementStatus(): void {
-    FileInput.value = "";
-    ImportDataButton.disabled = false;
-    ViewAllResultsButton.disabled = true;
-    FilterSelectionButton.disabled = true;
-    SelectAllButton.disabled = true;
-    SaveLocalStorageButton.disabled = true;
-    DownloadCSVButtonLayer.disabled = true;
-    DownloadConfigurationButtonLayer.disabled = true;
-    DeleteLocalStorageButton.disabled = true;
-    ClearTableButton.disabled = true;
+  FileInput.value = "";
+  ImportDataButton.disabled = true;
+  ViewAllResultsButton.disabled = true;
+  FilterSelectionButton.disabled = true;
+  SelectAllButton.disabled = true;
+  SaveLocalStorageButton.disabled = true;
+  DownloadCSVButtonLayer.disabled = true;
+  DownloadConfigurationButtonLayer.disabled = true;
+  DeleteLocalStorageButton.disabled = true;
+  ClearTableButton.disabled = true;
 }
 
 /**
  * Input values and button statuses when arriving/reloading to the plot page.
  */
 export function ElementStatusPlots(): void {
-    FileInput.value = "";
-    ViewPlotsButton.disabled = false;
+  FileInput.value = "";
+  ViewPlotsButton.disabled = false;
 }
 
 /**
  * Button statuses when the table is displayed.
  */
 export function ElementStatusWithTable() {
-    SelectAllButton.disabled = false;
-    FilterSelectionButton.disabled = false;
-    SaveLocalStorageButton.disabled = false;
-    DownloadConfigurationButtonLayer.disabled = false;
-    DownloadCSVButtonLayer.disabled = false;
-    DeleteLocalStorageButton.disabled = false;
-    ClearTableButton.disabled = false;
+  SelectAllButton.disabled = false;
+  FilterSelectionButton.disabled = false;
+  SaveLocalStorageButton.disabled = false;
+  DownloadConfigurationButtonLayer.disabled = false;
+  DownloadCSVButtonLayer.disabled = false;
+  DeleteLocalStorageButton.disabled = false;
+  ClearTableButton.disabled = false;
 }
