bdbe64ba2be7d6999b9f78baffe9b108fe413138
Oscar <91684376+Oscar-97@users.noreply.github.com>
2023-03-30 16:21:14 +0300

    Formatting.

--- Src/Scripts/Chart/InitializePlot.ts
+++ Src/Scripts/Chart/InitializePlot.ts
@@ -1,10 +1,7 @@
 import { CreateChart } from "./CreateChart";
 import { SelectAllButton } from "../Elements/Elements";
 
-export function InitializePlots(
-  ResultsData: any[]
-): void {
-
+export function InitializePlots(ResultsData: any[]): void {
   /**
    * @param CreateChart Display the data in a plot when clicking on the view plots button.
    */
