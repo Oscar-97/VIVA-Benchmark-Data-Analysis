65eb55b5986937770af82832f0642c03dc50704e
Oscar <91684376+Oscar-97@users.noreply.github.com>
2023-04-17 16:54:35 +0300

    Replaced parseFloat with BigNumber.

--- Src/Scripts/DataProcessing/CalculateResults.ts
+++ Src/Scripts/DataProcessing/CalculateResults.ts
@@ -1,17 +1,16 @@
 import * as math from "mathjs";
+//import { bignumber } from "mathjs";
+
 // Problem - Direction
 // https://github.com/coin-or/Paver/blob/783a6f5d0d3782a168d0ef529d01bcbda91ea8a4/src/paver/readgamstrace.py#L261-L262
 export function CalculateDirection(Direction: number | string): number {
   if (Direction === "" || Direction === "NA") {
-    Direction = 1;
+    return 1;
   } else {
-    if (typeof Direction === "number") {
-      Direction = 1 - 2 * Direction;
-    } else if (typeof Direction === "string") {
-      Direction = 1 - 2 * parseFloat(Direction);
-    }
+    const bigDirection = math.bignumber(Direction);
+    const result = math.bignumber(1).minus(bigDirection.times(2));
+    return result.toNumber();
   }
-  return Direction;
 }
 
 // Problem & Solver - Primal Bound
@@ -22,8 +21,8 @@ export function CalculatePrimalBound(
 ): number {
   if (PrimalBound === "" || PrimalBound === "NA") {
     PrimalBound = Direction * Infinity;
-  } else if (typeof PrimalBound === "string") {
-    PrimalBound = parseFloat(PrimalBound);
+  } else {
+    PrimalBound = math.bignumber(PrimalBound).toNumber();
   }
   return PrimalBound;
 }
@@ -37,7 +36,7 @@ export function CalculateDualBound(
   if (DualBound === "" || DualBound === "NA") {
     DualBound = -1 * Direction * Infinity;
   } else if (typeof DualBound === "string") {
-    DualBound = parseFloat(DualBound);
+    DualBound = math.bignumber(DualBound).toNumber();
   }
   return DualBound;
 }
@@ -83,30 +82,30 @@ export function SetTermStatus(TerminationStatus: number | string): string {
 // https://github.com/coin-or/Paver/blob/783a6f5d0d3782a168d0ef529d01bcbda91ea8a4/src/paver/utils.py#L46-L59
 export function CalculateGap(a: number, b: number, tol = 1e-9): number {
   // Check if the values are equal within tolerance
-  if (a === b || Math.abs(a - b) <= tol) {
+  if (a === b || math.abs(a - b) <= tol) {
     return 0.0;
   }
 
   // Check if either value is close to zero or infinity, or if the values have opposite signs
   if (
-    Math.abs(a) <= tol ||
-    Math.abs(b) <= tol ||
-    Math.abs(a) >= Infinity ||
-    Math.abs(b) >= Infinity ||
+    math.abs(a) <= tol ||
+    math.abs(b) <= tol ||
+    math.abs(a) >= Infinity ||
+    math.abs(b) >= Infinity ||
     a * b < 0.0
   ) {
     return Infinity;
   }
 
   // Compute and return the gap between the values
-  return (a - b) / Math.min(Math.abs(a), Math.abs(b));
+  return (a - b) / math.min(math.abs(a), math.abs(b));
 }
 
 // Solver - Gap[%]
 // https://github.com/coin-or/Paver/blob/783a6f5d0d3782a168d0ef529d01bcbda91ea8a4/src/paver/utils.py#L21-L39
 export function CalculateGapPercentage(a: number, b: number): number {
   let Gap: number;
-  if (Math.abs(a) >= Infinity || Math.abs(b) >= Infinity) {
+  if (math.abs(a) >= Infinity || math.abs(b) >= Infinity) {
     if (a === b) {
       Gap = 0.0;
       return Gap;
@@ -115,7 +114,7 @@ export function CalculateGapPercentage(a: number, b: number): number {
       return Gap;
     }
   } else {
-    Gap = (a - b) / Math.max(Math.abs(a), Math.abs(b), 1.0);
+    Gap = (a - b) / math.max(math.abs(a), math.abs(b), 1.0);
     return Gap;
   }
 }
@@ -145,7 +144,7 @@ export function AnalyzeDataByCategory(
    */
   const SolverTimes: { [SolverName: string]: number[] } = ResultsData.reduce(
     (acc, curr) => {
-      const parsedValue = parseFloat(curr[Category]);
+      const parsedValue = math.bignumber(curr[Category]);
 
       if (isFinite(parsedValue)) {
         if (!acc[curr.SolverName]) {
@@ -216,7 +215,7 @@ export function ExtractAllSolverTimes(TrcData: object[]) {
       if (!acc[obj["SolverName"]]) {
         acc[obj["SolverName"]] = [];
       }
-      const time = parseFloat(obj["Time[s]"]);
+      const time = math.bignumber(obj["Time[s]"]).toNumber();
       if (!isNaN(time)) {
         acc[obj["SolverName"]].push(time);
       }
