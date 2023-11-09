62df95900eb83b255812fe2ed4dcbfec83fe85de
Oscar <91684376+Oscar-97@users.noreply.github.com>
2023-11-10 00:43:30 +0200

    Updated UI tests with new alert and header names.

--- Tests/UI.test.ts
+++ Tests/UI.test.ts
@@ -45,7 +45,8 @@ describe("UI tests", () => {
 		expect(isVisible).toBeTruthy();
 
 		const notificationText = await notificationElement?.innerText();
-		expect(notificationText).toContain(expectedText);
+		const normalizedText = notificationText?.trim().replace(/\s+/g, " ");
+		expect(normalizedText).toContain(expectedText);
 	}
 
 	async function UploadFile(page: Page, fileNames: string[]): Promise<void> {
@@ -155,7 +156,10 @@ describe("UI tests", () => {
 				"./TestData/TraceFiles/scipALL.trc",
 				"./TestData/TraceFiles/pavitoALL.trc"
 			]);
-			await RunTableOperations(page, "Benchmark file succesfully loaded!");
+			await RunTableOperations(
+				page,
+				"Benchmarks loaded with following files: shotALL.trc, scipALL.trc, pavitoALL.trc"
+			);
 		}, 60000);
 
 		test("Instance information and best known bound values files", async () => {
@@ -172,7 +176,10 @@ describe("UI tests", () => {
 
 		test("Handle JSON-file", async () => {
 			await UploadFile(page, ["./TestData/UserConfiguration.json"]);
-			await RunTableOperations(page, "Benchmark file succesfully loaded!");
+			await RunTableOperations(
+				page,
+				"Benchmarks loaded with following files: UserConfiguration.json"
+			);
 		}, 60000);
 
 		test("Save to, load from and remove local storage", async () => {
@@ -180,7 +187,10 @@ describe("UI tests", () => {
 				localStorage.clear();
 			});
 			await UploadFile(page, ["./TestData/TraceFiles/shotALL.trc"]);
-			await RunTableOperations(page, "Benchmark file succesfully loaded!");
+			await RunTableOperations(
+				page,
+				"Benchmarks loaded with following files: shotALL.trc"
+			);
 			await WaitForElementAndClick(page, "#saveLocalStorageButton");
 			await page.reload();
 			await page.waitForTimeout(1000);
@@ -225,7 +235,10 @@ describe("UI tests", () => {
 
 		test("Select rows and filter table", async () => {
 			await UploadFile(page, ["./TestData/TraceFiles/shotALL.trc"]);
-			await RunTableOperations(page, "Benchmark file succesfully loaded!");
+			await RunTableOperations(
+				page,
+				"Benchmarks loaded with following files: shotALL.trc"
+			);
 			const rowSelectors = [
 				"//html/body/div[4]/div/div[3]/div/div/div[2]/table/tbody/tr[1]",
 				"//html/body/div[4]/div/div[3]/div/div/div[2]/table/tbody/tr[2]",
@@ -257,7 +270,10 @@ describe("UI tests", () => {
 
 		test("Button status after viewing a table", async () => {
 			await UploadFile(page, ["./TestData/TraceFiles/shotALL.trc"]);
-			await RunTableOperations(page, "Benchmark file succesfully loaded!");
+			await RunTableOperations(
+				page,
+				"Benchmarks loaded with following files: shotALL.trc"
+			);
 			await page.waitForTimeout(5000);
 			const buttonIDs = [
 				"#filterSelectionButton",
@@ -277,7 +293,10 @@ describe("UI tests", () => {
 
 		test("Download data", async () => {
 			await UploadFile(page, ["./TestData/TraceFiles/shotALL.trc"]);
-			await RunTableOperations(page, "Benchmark file succesfully loaded!");
+			await RunTableOperations(
+				page,
+				"Benchmarks loaded with following files: shotALL.trc"
+			);
 
 			page.on("download", async (download: Download) => {
 				const downloadPath: string = path.join(
@@ -294,7 +313,10 @@ describe("UI tests", () => {
 
 		test("Sort table and hide columns", async () => {
 			await UploadFile(page, ["./TestData/TraceFiles/shotALL.trc"]);
-			await RunTableOperations(page, "Benchmark file succesfully loaded!");
+			await RunTableOperations(
+				page,
+				"Benchmarks loaded with following files: shotALL.trc"
+			);
 			await WaitForElementAndClick(
 				page,
 				"//html/body/div[4]/div/div[3]/div/div/div[1]/div/table/thead/tr/th[1]"
@@ -324,12 +346,15 @@ describe("UI tests", () => {
 				"//html/body/div[4]/div/div[3]/div/div/div[1]/div/table/thead/tr/th[1]"
 			);
 			const firstHeaderValueText = await firstHeaderValue?.innerText();
-			expect(firstHeaderValueText).toBe("InputFileName");
+			expect(firstHeaderValueText).toBe("Problem");
 		}, 60000);
 
 		test("Use pagination on the table", async () => {
 			await UploadFile(page, ["./TestData/TraceFiles/shotALL.trc"]);
-			await RunTableOperations(page, "Benchmark file succesfully loaded!");
+			await RunTableOperations(
+				page,
+				"Benchmarks loaded with following files: shotALL.trc"
+			);
 
 			await WaitForElementAndClick(
 				page,
@@ -353,7 +378,10 @@ describe("UI tests", () => {
 
 		test("Search in the displayed data", async () => {
 			await UploadFile(page, ["./TestData/TraceFiles/shotALL.trc"]);
-			await RunTableOperations(page, "Benchmark file succesfully loaded!");
+			await RunTableOperations(
+				page,
+				"Benchmarks loaded with following files: shotALL.trc"
+			);
 			await page
 				.locator("//html/body/div[4]/div/div[2]/div[2]/div/label/input")
 				.type("watercontamination");
@@ -378,7 +406,7 @@ describe("UI tests", () => {
 			await CheckNotification(
 				page,
 				"#alertNotification",
-				"Benchmark file succesfully loaded!"
+				"Benchmarks loaded with following files: shotALL.trc"
 			);
 			await WaitForElementAndClick(page, "#viewPlotsButton");
 
