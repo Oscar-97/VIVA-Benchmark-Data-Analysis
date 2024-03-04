95fbb0b836a553858db5a8b9a3fbda10954511d1
Oscar <91684376+Oscar-97@users.noreply.github.com>
2024-03-04 23:43:36 +0200

    Class removal on already displayed alerts.

--- Src/Scripts/Elements/DisplayAlertNotification.ts
+++ Src/Scripts/Elements/DisplayAlertNotification.ts
@@ -13,24 +13,29 @@ import $ from "jquery";
  * @param message - The message to be displayed in the notification.
  */
 export function DisplayAlertNotification(message: string): void {
-	alertNotification.classList.add("alert-success");
-	alertMessage.innerText = message;
-	alertIcon.innerHTML = "<i class='bi bi-check-circle-fill'></i>";
+	try {
+		// Remove other alert classes if notification is already displayed and it is triggered again.
+		alertNotification.classList.remove("alert-warning", "alert-danger");
+	} finally {
+		alertNotification.classList.add("alert-success");
+		alertMessage.innerText = message;
+		alertIcon.innerHTML = "<i class='bi bi-check-circle-fill'></i>";
 
-	$(alertNotification)
-		.css({ display: "block", opacity: 0 })
-		.animate({ opacity: 1 }, 125);
+		$(alertNotification)
+			.css({ display: "block", opacity: 0 })
+			.animate({ opacity: 1 }, 125);
 
-	closeAlertButton.addEventListener("click", function () {
-		alertNotification.style.display = "none";
-	});
-
-	setTimeout(() => {
-		$(alertNotification).animate({ opacity: 0 }, 250, function () {
+		closeAlertButton.addEventListener("click", function () {
 			alertNotification.style.display = "none";
-			alertNotification.classList.remove("alert-success");
 		});
-	}, 10000);
+
+		setTimeout(() => {
+			$(alertNotification).animate({ opacity: 0 }, 250, function () {
+				alertNotification.style.display = "none";
+				alertNotification.classList.remove("alert-success");
+			});
+		}, 10000);
+	}
 }
 
 /**
@@ -40,24 +45,28 @@ export function DisplayAlertNotification(message: string): void {
  * @param message - The message to be displayed in the notification.
  */
 export function DisplayWarningNotification(message: string): void {
-	alertNotification.classList.add("alert-warning");
-	alertMessage.innerText = message;
-	alertIcon.innerHTML = "<i class='bi bi-exclamation-triangle-fill'></i>";
+	try {
+		alertNotification.classList.remove("alert-success", "alert-danger");
+	} finally {
+		alertNotification.classList.add("alert-warning");
+		alertMessage.innerText = message;
+		alertIcon.innerHTML = "<i class='bi bi-exclamation-triangle-fill'></i>";
 
-	$(alertNotification)
-		.css({ display: "block", opacity: 0 })
-		.animate({ opacity: 1 }, 125);
+		$(alertNotification)
+			.css({ display: "block", opacity: 0 })
+			.animate({ opacity: 1 }, 125);
 
-	closeAlertButton.addEventListener("click", function () {
-		alertNotification.style.display = "none";
-	});
-
-	setTimeout(() => {
-		$(alertNotification).animate({ opacity: 0 }, 250, function () {
+		closeAlertButton.addEventListener("click", function () {
 			alertNotification.style.display = "none";
-			alertNotification.classList.remove("alert-warning");
 		});
-	}, 5000);
+
+		setTimeout(() => {
+			$(alertNotification).animate({ opacity: 0 }, 250, function () {
+				alertNotification.style.display = "none";
+				alertNotification.classList.remove("alert-warning");
+			});
+		}, 5000);
+	}
 }
 
 /**
@@ -67,22 +76,26 @@ export function DisplayWarningNotification(message: string): void {
  * @param message - The message to be displayed in the notification.
  */
 export function DisplayErrorNotification(message: string): void {
-	alertNotification.classList.add("alert-danger");
-	alertMessage.innerText = message;
-	alertIcon.innerHTML = "<i class='bi bi-exclamation-triangle-fill'></i>";
+	try {
+		alertNotification.classList.remove("alert-success", "alert-warning");
+	} finally {
+		alertNotification.classList.add("alert-danger");
+		alertMessage.innerText = message;
+		alertIcon.innerHTML = "<i class='bi bi-exclamation-triangle-fill'></i>";
 
-	$(alertNotification)
-		.css({ display: "block", opacity: 0 })
-		.animate({ opacity: 1 }, 125);
+		$(alertNotification)
+			.css({ display: "block", opacity: 0 })
+			.animate({ opacity: 1 }, 125);
 
-	closeAlertButton.addEventListener("click", function () {
-		alertNotification.style.display = "none";
-	});
-
-	setTimeout(() => {
-		$(alertNotification).animate({ opacity: 0 }, 250, function () {
+		closeAlertButton.addEventListener("click", function () {
 			alertNotification.style.display = "none";
-			alertNotification.classList.remove("alert-danger");
 		});
-	}, 5000);
+
+		setTimeout(() => {
+			$(alertNotification).animate({ opacity: 0 }, 250, function () {
+				alertNotification.style.display = "none";
+				alertNotification.classList.remove("alert-danger");
+			});
+		}, 5000);
+	}
 }
